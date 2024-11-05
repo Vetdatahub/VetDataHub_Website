8a4dd403dd271e160c95d594364f27a0e47791d2
hamub <hbutau35@gmail.com>
2024-11-05 07:34:21 +0200

    add custom user model

--- vetdatahub/settings.py
+++ vetdatahub/settings.py
@@ -130,3 +130,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Custom User Model
+# https://docs.djangoproject.com/en/5.1/topics/auth/customizing/#using-a-custom-user-model-when-starting-a-project
+
+AUTH_USER_MODEL = "registration.User"
