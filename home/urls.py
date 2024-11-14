2bccc7916dba5afe7cf794266cd751161263e91c
Johanan Oppong Amoateng <88411614+JohananOppongAmoateng@users.noreply.github.com>
2024-11-14 00:06:35 +0000

    home views forms  and urls

--- home/urls.py
+++ home/urls.py
@@ -1,1 +1,7 @@
 from django.urls import path
+
+from home.views import home
+app_name = 'home'
+urlpatterns = [
+    path("",home,name='home'),
+]
--- home/views.py
+++ home/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse
+from home.forms import ContactForm
 
 # Create your views here.
+def home(request):
+    if request.method == 'GET':
+        form = ContactForm()
+    else:
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data["name"]
+            email = form.cleaned_data["email"]
+            subject = form.cleaned_data["subject"]
+            message = form.cleaned_data["message"]
+            try:
+                send_mail(subject, message, email, ['vetdatahub@gmail.com'], name)
+            except BadHeaderError:
+                return HttpResponse("Invalid header found")
+            return redirect("home")
+
+    return render(request,"home/home.html",context={"form": form})
+
