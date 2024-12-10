d3393fb443536f4a56dd41c204ecbdba03a4197d
Johanan Oppong Amoateng <88411614+JohananOppongAmoateng@users.noreply.github.com>
2024-12-10 19:10:12 +0000

    settings

--- vetdatahub/settings_staging.py
+++ vetdatahub/settings_staging.py
@@ -2,8 +2,7 @@ import os
 from .settings import *
 from dotenv import load_dotenv
 
-
-load_dotenv(os.path.join(BASE_DIR, ".env"))
+load_dotenv()
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 ALLOWED_HOSTS = ALLOWED_HOSTS + [os.environ.get('ALLOWED_HOSTS')]
