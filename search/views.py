e0f2729afaf200983917902aaa59f2ad387bc126
Johanan Oppong Amoateng <88411614+JohananOppongAmoateng@users.noreply.github.com>
2024-12-10 15:03:47 +0000

    templates

--- search/views.py
+++ search/views.py
@@ -1,3 +1,30 @@
+from django.db.models import Q
 from django.shortcuts import render
+from datasets.models import Dataset
+from community.models import Discussion, Comment
 
-# Create your views here.
+def search(request):
+    query = request.GET.get("q", "").strip()
+    dataset_results, discussion_results, comment_results = [], [], []
+    if query:
+        # Separate queries for each model
+        dataset_results = Dataset.objects.filter(
+            Q(name__icontains=query) | Q(description__icontains=query)
+        )
+        discussion_results = Discussion.objects.filter(
+            Q(title__icontains=query) | Q(description__icontains=query)
+        )
+        comment_results = Comment.objects.filter(
+            Q(content__icontains=query)
+        )
+
+    return render(
+        request,
+        "search/search_results.html",
+        {
+            "query": query,
+            "dataset_results": dataset_results,
+            "discussion_results": discussion_results,
+            "comment_results": comment_results,
+        },
+    )
